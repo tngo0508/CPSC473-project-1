e5f055b3d97990db2c2ce66731e7ae349fd7992c
Thomas Ngo <tngo0508@gmail.com>
2018-03-26 17:33:18 -0700

    create main.html and insert user's input to deployd

---
+++ quizcreator-backend/public/scripts/main.js
@@ -0,0 +1,24 @@
+(function(window) {
+  "use strict";
+  var FORM_SELECTOR = "[data-user-quiz]";
+  var App = window.App;
+  var FormHandler = App.FormHandler;
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  var dpd = window.dpd;
+
+  formHandler.addSubmitHandler(function(data) {
+    dpd.quiz.post({
+      question: data["question"],
+      optionA: data["optionA"],
+      optionB: data["optionB"],
+      optionC: data["optionC"]
+    }, function(result, error) {
+      if (error) {
+        alert(JSON.stringify(error));
+      } else {
+        location.href = "/main.html";
+      }
+    });
+  });
+
+})(window);
