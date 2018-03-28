9c4da0847e35f9b5b5712205324e48e87ba07e45
Thomas Ngo <tngo0508@gmail.com>
2018-03-27 17:31:46 -0700

    fix validating input before inserting into deployd for main.js

--- quizcreator-backend/public/scripts/main.js
+++ quizcreator-backend/public/scripts/main.js
@@ -25,20 +25,29 @@
       if (error) {
         alert(error.message);
       } else {
-        dpd.quiz.post({
-          username: results.username,
-          question: data["question"],
-          optionA: data["optionA"],
-          optionB: data["optionB"],
-          optionC: data["optionC"],
-          answer: data["answer"]
-        }, function(result, error) {
-          if (error) {
-            alert(JSON.stringify(error));
-          } else {
-            // location.href = "/main.html";
-          }
-        });
+        var question = data["question"],
+          optionA = data["optionA"],
+          optionB = data["optionB"],
+          optionC = data["optionC"],
+          answer = data["answer"];
+        if (question || optionA || optionB || optionC || answer) {
+          dpd.quiz.post({
+            username: results.username,
+            question: question,
+            optionA: optionA,
+            optionB: optionB,
+            optionC: optionC,
+            answer: answer
+          }, function(result, error) {
+            if (error) {
+              alert(JSON.stringify(error));
+            } else {
+              // location.href = "/main.html";
+            }
+          });
+        } else {
+          console.log("No question is added.");
+        }
       }
     });
   });
